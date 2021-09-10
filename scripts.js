60e4eb9c7791bac928dacb2f3ba795b58899e6a4
sbtries <sarahbtracy17@gmail.com>
2021-09-10 14:21:45 -0400

    updating with new profile

---
+++ scripts.js
@@ -0,0 +1,15 @@
+console.log('hello')
+let app = new Vue({
+    el: '#app',
+    data: {
+        hover: false,
+    },
+    methods: {
+        methodName: function(){
+            this.message = 'you clicked the button'
+        },
+        methodA: function(){
+           return NamedNodeMap;
+              }
+        }
+    })
